0b18457e767fb3539044662be954b3c15bc663ec
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-08 16:05:19 -0400

    feat: ✨ add  models

---
+++ src/controller/model/request/user_request.go
@@ -0,0 +1,15 @@
+package request
+
+type UserRequest struct {
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=6,containsany=!@#$%*"`
+	Name      string `json:"name" binding:"required,min=4,max=100"`
+	Phone     string `json:"phone" binding:"required,min=1,max=140"`
+	DateBirth string `json:"dateBirth" binding:"required,min=1,max=140"`
+}
+
+type UserUpdateRequest struct {
+	Name      string `json:"name" binding:"omitempty,min=4,max=100"`
+	Phone     string `json:"phone" binding:"required,min=1,max=140"`
+	DateBirth string `json:"dateBirth" binding:"required,min=1,max=140"`
+}
