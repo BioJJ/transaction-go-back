1c4e35c33851e33acb494db17977e9103684262a
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-08 16:39:58 -0400

    feat: ✨ add Logger

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -3,18 +3,24 @@ package controller
 import (
 	"fmt"
 
+	"github.com/BioJJ/transaction-go-back/src/config/logger"
 	"github.com/BioJJ/transaction-go-back/src/config/validation"
 	"github.com/BioJJ/transaction-go-back/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
 
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
+
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		// logger.Error("Error trying to validate user info", err,
-		// 	zap.String("journey", "createUser"))
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
