2f368fa2f2cd249df94dd281d0c043698b1367e3
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-09 23:43:22 -0400

    feat: ✨ call Delete User

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -11,6 +11,6 @@ func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInte
 	r.GET("/users/:userEmail/email", userController.FindUserByEmail)
 	r.POST("/users", userController.CreateUser)
 	r.PUT("/users/:userId", userController.UpdateUser)
-	r.DELETE("/users/delete/:id", userController.DeleteUser)
+	r.DELETE("/users/:userId", userController.DeleteUser)
 
 }
--- src/model/service/delete_user.go
+++ src/model/service/delete_user.go
@@ -6,9 +6,23 @@ import (
 	"go.uber.org/zap"
 )
 
-func (*userDomainService) DeleteUser(string) *rest_err.RestErr {
+func (ud *userDomainService) DeleteUser(
+	userId string) *rest_err.RestErr {
 
-	logger.Info("Init DeleteUser Model", zap.String("journey", "DeleteUser"))
+	logger.Info("Init deleteUser model.",
+		zap.String("journey", "deleteUser"))
 
+	err := ud.userRepository.DeleteUser(userId)
+	if err != nil {
+		logger.Error("Error trying to call repository",
+			err,
+			zap.String("journey", "deleteUser"))
+		return err
+	}
+
+	logger.Info(
+		"deleteUser service executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "deleteUser"))
 	return nil
 }
