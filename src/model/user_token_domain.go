a996b357bfbedaec3337277eec9802d03ff4fb87
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-10 02:11:29 -0400

    feat: ✨ add VerifyTokenMiddleware in Controller

--- src/model/user_token_domain.go
+++ src/model/user_token_domain.go
@@ -3,9 +3,12 @@ package model
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/BioJJ/transaction-go-back/src/config/logger"
 	"github.com/BioJJ/transaction-go-back/src/config/rest_err"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
@@ -34,4 +37,75 @@ func (ud *userDomain) GenerateToken() (string, *rest_err.RestErr) {
 	}
 
 	return tokenString, nil
+}
+
+func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
+	secret := os.Getenv(JWT_SECRET_KEY)
+
+	token, err := jwt.Parse(RemoveBearerPrefix(tokenValue), func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+			return []byte(secret), nil
+		}
+
+		return nil, rest_err.NewBadRequestError("invalid token")
+	})
+	if err != nil {
+		return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+	}
+
+	return &userDomain{
+		id:        claims["id"].(string),
+		email:     claims["email"].(string),
+		name:      claims["name"].(string),
+		phone:     claims["phone"].(string),
+		dateBirth: claims["dateBirth"].(string),
+	}, nil
+}
+
+func VerifyTokenMiddleware(c *gin.Context) {
+	secret := os.Getenv(JWT_SECRET_KEY)
+	tokenValue := RemoveBearerPrefix(c.Request.Header.Get("Authorization"))
+
+	token, err := jwt.Parse(tokenValue, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+			return []byte(secret), nil
+		}
+
+		return nil, rest_err.NewBadRequestError("invalid token")
+	})
+	if err != nil {
+		errRest := rest_err.NewUnauthorizedRequestError("invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		errRest := rest_err.NewUnauthorizedRequestError("invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	userDomain := userDomain{
+		id:        claims["id"].(string),
+		email:     claims["email"].(string),
+		name:      claims["name"].(string),
+		phone:     claims["phone"].(string),
+		dateBirth: claims["dateBirth"].(string),
+	}
+	logger.Info(fmt.Sprintf("User authenticated: %#v", userDomain))
+}
+
+func RemoveBearerPrefix(token string) string {
+	if strings.HasPrefix(token, "Bearer ") {
+		token = strings.TrimPrefix(token, "Bearer ")
+	}
+	return token
 }
