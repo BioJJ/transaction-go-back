4013bf8d51e957cc1234f6af1d710ce45c72fdeb
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-09 22:04:51 -0400

    refactor: ♻️ clean code

--- src/model/service/user_interface.go
+++ src/model/service/user_interface.go
@@ -3,17 +3,21 @@ package service
 import (
 	"github.com/BioJJ/transaction-go-back/src/config/rest_err"
 	"github.com/BioJJ/transaction-go-back/src/model"
+	"github.com/BioJJ/transaction-go-back/src/model/repository"
 )
 
 type userDomainService struct {
+	userRepository repository.UserRepository
 }
 
-func NewUserDomainService() UserDomainService {
-	return &userDomainService{}
+func NewUserDomainService(
+	userRepository repository.UserRepository,
+) UserDomainService {
+	return &userDomainService{userRepository}
 }
 
 type UserDomainService interface {
-	CreateUser(model.UserDomainInterface) *rest_err.RestErr
+	CreateUser(model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr)
 	UpdateUser(string, model.UserDomainInterface) *rest_err.RestErr
 	FindUser(string) (*model.UserDomainInterface, *rest_err.RestErr)
 	DeleteUser(string) *rest_err.RestErr
--- src/model/user_domain_interface.go
+++ src/model/user_domain_interface.go
@@ -2,33 +2,32 @@ package model
 
 // import "github.com/BioJJ/transaction-go-back/src/config/rest_err"
 
-// type UserDomainInterface interface {
-// 	GetEmail() string
-// 	GetPassword() string
-// 	GetAge() int8
-// 	GetName() string
-// 	GetPhone() string
-// 	GetDateBirth() string
-// 	GetID() string
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetName() string
+	GetPhone() string
+	GetDateBirth() string
+	GetID() string
 
-// 	SetID(string)
+	SetID(string)
 
-// 	EncryptPassword()
-// 	GenerateToken() (string, *rest_err.RestErr)
-// }
+	EncryptPassword()
+	// GenerateToken() (string, *rest_err.RestErr)
+}
 
-// func NewUserDomain(
-// 	email, password, name string,
-// 	phone, dateBirth string,
-// ) UserDomainInterface {
-// 	return &userDomain{
-// 		email:    email,
-// 		password: password,
-// 		name:     name,
-// 		phone:      phone,
-// 		dateBirth: dateBirth,
-// 	}
-// }
+func NewUserDomain(
+	email, password, name string,
+	phone, dateBirth string,
+) UserDomainInterface {
+	return &userDomain{
+		email:     email,
+		password:  password,
+		name:      name,
+		phone:     phone,
+		dateBirth: dateBirth,
+	}
+}
 
 // func NewUserLoginDomain(
 // 	email, password string,
