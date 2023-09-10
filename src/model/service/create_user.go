9d97d8d89de66f599aa8ace08094f22d9104ebda
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-09 23:47:37 -0400

    fix: 🐛bug fix Email is already registered in another account

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -13,6 +13,11 @@ func (ud *userDomainService) CreateUser(
 
 	logger.Info("Init createUser Model", zap.String("journey", "createUser"))
 
+	user, _ := ud.FindUserByEmailServices(userDomain.GetEmail())
+	if user != nil {
+		return nil, rest_err.NewBadRequestError("Email is already registered in another account")
+	}
+
 	userDomain.EncryptPassword()
 
 	userDomainRepository, err := ud.userRepository.CreateUser(userDomain)
