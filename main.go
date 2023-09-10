57275be89799a27e87c3cec9faa7972b8b1304a2
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-09 20:18:09 -0400

    chore(deps): 🔗 add dependencies

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/BioJJ/transaction-go-back/src/config/database/mongodb"
 	"github.com/BioJJ/transaction-go-back/src/controller"
 	"github.com/BioJJ/transaction-go-back/src/controller/routes"
 	"github.com/BioJJ/transaction-go-back/src/model/service"
@@ -18,6 +20,14 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	fmt.Println("variavel de ambiente ==> ", os.Getenv("TEST"))
+
+	database, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatalf(
+			"Error trying to connect to database, error=%s \n",
+			err.Error())
+		return
+	}
 
 	router := gin.Default()
 
