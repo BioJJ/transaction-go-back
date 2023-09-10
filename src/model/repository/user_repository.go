2d7ec053c5e2c2f379165bae0930177e2d0d025c
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-09 22:04:06 -0400

    feat:  ✨ update repository user

---
+++ src/model/repository/user_repository.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"github.com/BioJJ/transaction-go-back/src/config/rest_err"
+	"github.com/BioJJ/transaction-go-back/src/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewUserRepository(
+	database *mongo.Database,
+) UserRepository {
+	return &userRepository{
+		database,
+	}
+}
+
+type userRepository struct {
+	databaseConnection *mongo.Database
+}
+
+type UserRepository interface {
+	CreateUser(
+		userDomain model.UserDomainInterface,
+	) (model.UserDomainInterface, *rest_err.RestErr)
+}
